4d3acdda472878fae5e2c9ba2f00084752d3030f
hagana98 <hagana98@students.rowan.edu>
2023-02-15 14:46:12 -0500

    Init LED, button, and timer

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -29,15 +29,25 @@ void main(){
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
 
-    // @TODO Initialize Button 2.3
+    // Configure RED LED on P1.0 as Output
+          P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+          P1DIR |= BIT0;                          // Set P1.0 to output direction
 
+
+          // @TODO Initialize Button 2.3
+          // Configure Button on P2.3 as input with pullup resistor
+          P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+          P2REN |= BIT3;                          // P2.3 pull-up register enable
+          P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+          P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
 
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
+    TB1CCTL0 = CCIE;
+    TB1CTL = TBSSEL_1 | MC_2;
 
 }
 
